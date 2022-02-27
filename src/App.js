bcf92798f4423947c264596056c696c0a315f448
Tasha <trn.tasha@gmail.com>
2022-02-27 09:59:03 -0600

    added boostrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      <button type="button" class="btn btn-primary">
+        Hello
+      </button>
+    </div>
+  );
+}
