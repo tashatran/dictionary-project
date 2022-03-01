b339917fa30c98e989df8a40333df8d9ecfaa306
Tasha <trn.tasha@gmail.com>
2022-02-28 21:37:02 -0600

    created dictionary component w/ search input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictonary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-
-      <button type="button" class="btn btn-primary">
-        Hello
-      </button>
+      <div className="container">
+        <Dictionary />
+        <footer className="App-footer">
+          <small>Coded by Tasha</small>
+        </footer>
+      </div>
     </div>
   );
 }
