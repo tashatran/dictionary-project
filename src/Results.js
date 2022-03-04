c92d15de1a1cdace6822db3b272aff0479a43f91
Tasha <trn.tasha@gmail.com>
2022-03-03 20:18:33 -0600

    added synonyms

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <h5>{props.results.phonetics[0].text}</h5>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
