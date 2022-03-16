e53793998b7faee3d18131354c5db7948cbd4cc8
Tasha <trn.tasha@gmail.com>
2022-03-15 21:16:18 -0500

    added alt

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function Photos(props) {
             return (
               <div className="col-3" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.tiny} className="img-fluid" />
+                  <img src={photo.src.tiny} className="img-fluid" alt="" />
                 </a>
               </div>
             );
